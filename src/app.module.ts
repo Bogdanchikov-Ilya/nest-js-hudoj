dff88a22d152ff394725d0542e586250be6618fc
Илья Богданчиков <test200373@gmail.com>
2022-10-03 18:00:27 +0300

    connet db postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ProductModule } from './product/product.module';
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-	imports: [AuthModule, ProductModule],
+	imports: [
+		ConfigModule.forRoot( { isGlobal: true }),
+		TypeOrmModule.forRoot({
+			type: 'postgres',
+			host: process.env.POSTGRES_HOST,
+			port: parseInt(<string>process.env.POSTGRES_PORT),
+			username: process.env.POSTGRES_USER,
+			password: process.env.POSTGRES_PASSWORD,
+			database: process.env.POSTGRES_DATABASE
+		}),
+		AuthModule, ProductModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
